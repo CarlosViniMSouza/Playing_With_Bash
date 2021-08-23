b6204e84146769c5a3e94aac3b2ca47feeef0506
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-08-23 14:43:57 -0300

    project with shellscript start

---
+++ exec.sh
@@ -0,0 +1,14 @@
+mkdir PlayGround_Shell
+cd PlayGround_Shell
+
+echo "print('Hello Guys! ✌️🤓')" >> HyPeople.py
+
+python HyPeople.py
+
+name="CarlosViniMSouza"
+editor="VS Code"
+
+echo "Created by: $name"
+echo "Edited in: $editor"
+
+# this is my first test with ShellScript !
